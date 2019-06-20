ec41038f1905801d18ca19f85a9ec56c69f6fa46
10sm <10sm@10thstreet.media>
2019-06-20 08:58:06 -0400

    Added conn.php

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ *
+ * conn.php
+ *
+ * Stores information that allows Federama to connect to the database.
+ *
+ * since Federama version 0.1
+ *
+ */
+
+define("DBHOST","");
+define("DBNAME","");
+define("DBUSER","");
+define("DBPASS","");
+define("TBLPREFIX","");
+define("VERSION","Federama 0.1");
+?>
