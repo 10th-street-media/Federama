b5556b6696529ebb0a818d12e5c0847b10d92e47
10sm <10sm@10thstreet.media>
2019-06-24 09:41:56 -0400

    Getting started with themes.

---
+++ pub/themes/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * pub/themes/index.php
+ *
+ * People shouldn't normally be in here, so they are redirected if the try to navigate here.
+ *
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Federama 0.1</title>
+<meta http-equiv="refresh" content="1 URL='../index.php'" />
+</head>
+<body>
+<!-- pub/themes/index.php -->
+<!-- This page just redirects the browser to ../index.php -->
+<!-- since Federama version 0.1 -->
+</body>
+</html>
