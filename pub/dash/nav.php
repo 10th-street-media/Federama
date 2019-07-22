9f3751295af34f845d73a4fe1d81b328c834fb68
10sm <10sm@10thstreet.media>
2019-07-22 08:50:45 -0400

    check user level, show submenus

--- pub/dash/nav.php
+++ pub/dash/nav.php
@@ -23,5 +23,87 @@
 				<a href="<?php echo $website_url."dash/bookmarks.php"; ?>" class="w3-bar-item w3-button" title="<?php echo _("Items you've liked or disliked."); ?>"><i class="fa fa-lg fa-bookmark"></i>&nbsp;<?php echo _("Noteworthy"); ?></a>
 				<a href="<?php echo $website_url."dash/media.php"; ?>" class="w3-bar-item w3-button" title="<?php echo _("Images, audio, and video on this instance."); ?>"><i class="fa fa-lg fa-file-image-o"></i>&nbsp;<?php echo _("Media"); ?></a>
 				<a href="<?php echo $website_url."dash/profile.php"; ?>" class="w3-bar-item w3-button" title="<?php echo _("Your profile."); ?>"><i class="fa fa-lg fa-user" ></i>&nbsp;<?php echo _("Profile"); ?></a>
-				<a href="<?php echo $website_url."dash/admin/index.php"; ?>" class="w3-bar-item w3-button" title="<?php echo _("Administrator dashboard"); ?>"><i class="fa fa-lg fa-wrench"></i>&nbsp;<?php echo _("Admin"); ?></a>
+<?php
+$adminq = "SELECT * FROM ".TBLPREFIX."users WHERE user_id=".$_COOKIE['id'];
+$adminquery = mysqli_query($dbconn,$adminq);
+while($adminopt = mysqli_fetch_assoc($adminquery)) {
+		$level	= $adminopt['user_level'];
+
+		if ($level == 'ADMINISTRATOR') {
+			echo "\t\t\t\t<a href=\"#\" class=\"w3-bar-item w3-button\" title=\""._('User list')."\" onclick=\"myUsersMenu()\"><i class=\"fa fa-lg fa-users\"></i>&nbsp;"._('Users')."&nbsp;<i class=\"fa fa-caret-down w3-right\"></i></a>\n";
+			echo "\t\t\t\t\t<div id=\"usersMenu\" class=\"w3-hide w3-white w3-card\">\n";
+			echo "\t\t\t\t\t\t<a href=\"".$website_url."dash/admin/users.php\" class=\"w3-bar-item w3-button\">"._('All users')."</a>\n";
+			echo "\t\t\t\t\t\t<a href=\"".$website_url."dash/admin/add-user.php\" class=\"w3-bar-item w3-button\">"._('Add user')."</a>\n";
+			echo "\t\t\t\t\t</div>\n";
+			echo "\t\t\t\t<a href=\"#\" class=\"w3-bar-item w3-button\" title=\""._('Administrator dashboard')."\" onclick=\"myAdminMenu()\"><i class=\"fa fa-lg fa-wrench\"></i>&nbsp;"._('Administrators')."&nbsp;<i class=\"fa fa-caret-down w3-right\"></i></a>\n";
+			echo "\t\t\t\t\t<div id=\"adminMenu\" class=\"w3-hide w3-white w3-card\">\n";
+			echo "\t\t\t\t\t\t<a href=\"".$website_url."dash/admin/configuration.php\" class=\"w3-bar-item w3-button\">"._('Website configuration')."</a>\n";
+			echo "\t\t\t\t\t\t<a href=\"#\" class=\"w3-bar-item w3-button\">"._('Some text')."</a>\n";
+			echo "\t\t\t\t\t</div>\n";
+		}
+
+		if ($level == 'MODERATOR' || 'ADMINISTRATOR') {
+			echo "\t\t\t\t<a href=\"#\" class=\"w3-bar-item w3-button\" title=\""._('Moderator dashboard')."\" onclick=\"myModMenu()\"><i class=\"fa fa-lg fa-check-square\"></i>&nbsp;"._('Moderators')."&nbsp;<i class=\"fa fa-caret-down w3-right\"></i></a>\n";
+			echo "\t\t\t\t\t<div id=\"modMenu\" class=\"w3-hide w3-white w3-card\">\n";
+			echo "\t\t\t\t\t\t<a href=\"".$website_url."dash/admin/moderation-queue.php\" class=\"w3-bar-item w3-button\">"._('Moderation queue')."</a>\n";
+			echo "\t\t\t\t\t</div>\n";
+		}
+
+		if ($level == 'TRANSLATOR' || 'ADMINISTRATOR') {
+			echo "\t\t\t\t<a href=\"#\" class=\"w3-bar-item w3-button\" title=\""._('Translator dashboard')."\" onclick=\"myL10nMenu()\"><i class=\"fa fa-lg fa-globe\"></i>&nbsp;"._('Translators')."&nbsp;<i class=\"fa fa-caret-down w3-right\"></i></a>\n";
+			echo "\t\t\t\t\t<div id=\"l10nMenu\" class=\"w3-hide w3-white w3-card\">\n";
+			echo "\t\t\t\t\t\t<a href=\"".$website_url."dash/admin/locales.php\" class=\"w3-bar-item w3-button\">"._('Locales')."</a>\n";
+			echo "\t\t\t\t\t</div>\n";
+		}
+}
+?>
 			</nav>
+			<script>
+			function myUsersMenu() {
+			  var x = document.getElementById("usersMenu");
+			  if (x.className.indexOf("w3-show") == -1) {
+			    x.className += " w3-show";
+			    x.previousElementSibling.className += " w3-theme-d1";
+			  } else {
+			    x.className = x.className.replace(" w3-show", "");
+			    x.previousElementSibling.className =
+			    x.previousElementSibling.className.replace(" w3-theme-d1", "");
+			  }
+			}
+
+			function myAdminMenu() {
+			  var x = document.getElementById("adminMenu");
+			  if (x.className.indexOf("w3-show") == -1) {
+			    x.className += " w3-show";
+			    x.previousElementSibling.className += " w3-theme-d1";
+			  } else {
+			    x.className = x.className.replace(" w3-show", "");
+			    x.previousElementSibling.className =
+			    x.previousElementSibling.className.replace(" w3-theme-d1", "");
+			  }
+			}
+
+			function myModMenu() {
+			  var x = document.getElementById("modMenu");
+			  if (x.className.indexOf("w3-show") == -1) {
+			    x.className += " w3-show";
+			    x.previousElementSibling.className += " w3-theme-d1";
+			  } else {
+			    x.className = x.className.replace(" w3-show", "");
+			    x.previousElementSibling.className =
+			    x.previousElementSibling.className.replace(" w3-theme-d1", "");
+			  }
+			}
+
+			function myL10nMenu() {
+			  var x = document.getElementById("l10nMenu");
+			  if (x.className.indexOf("w3-show") == -1) {
+			    x.className += " w3-show";
+			    x.previousElementSibling.className += " w3-theme-d1";
+			  } else {
+			    x.className = x.className.replace(" w3-show", "");
+			    x.previousElementSibling.className =
+			    x.previousElementSibling.className.replace(" w3-theme-d1", "");
+			  }
+			}
+			</script>
