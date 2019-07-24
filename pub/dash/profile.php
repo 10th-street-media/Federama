ec35da0c99d8bf9a918b9a1e5cd9cc6609771919
10sm <10sm@10thstreet.media>
2019-07-24 14:28:27 -0400

    Planning ahead

--- pub/dash/profile.php
+++ pub/dash/profile.php
@@ -21,11 +21,40 @@ include "nav.php";
 			<article class="w3-content w3-padding">
 
 				<h2 class="w3-padding"><?php echo _("Profile"); ?></h2>
+				<p>The nav menu will be changing soon to something more like:
 				<ul>
-					<li>Edit</li>
-					<li>Import</li>
-					<li>Export</li>
-					<li>Delete</li>
+					<li>View profile
+						<ul>
+							<li>Edit</li>
+						</ul>
+					</li>
+					<li>Messages
+						<ul>
+							<li>Inbox</li>
+							<li>Outbox</li>
+						</ul>
+					</li>
+					<li>Contacts
+						<ul>
+							<li>Following</li>
+							<li>Followers</li>
+							<li>Patrons <i>(ideas)</i></li>
+						</ul>
+					</li>
+					<li>Noteworthy
+						<ul>
+							<li>Liked</li>
+							<li>Boosted</li>
+							<li>Disliked <i>(optional)</i></li>
+						</ul>
+					</li>
+					<li>Tools
+						<ul>
+							<li>Import profile</li>
+							<li>Export profile</li>
+							<li>Delete profile</li>
+						</ul>
+					</li>
 				</ul>
 
 			</article> <!-- end article (It's not really an article, but it serves the same purpose.) -->
