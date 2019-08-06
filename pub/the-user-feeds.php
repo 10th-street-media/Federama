eb0a9994325d743b2df08972cba57f74c572b7d3
10sm <10sm@10thstreet.media>
2019-08-06 08:10:36 -0400

    Got some stuff working. Lays foundation for ActivityPub.

--- pub/the-user-feeds.php
+++ pub/the-user-feeds.php
@@ -18,15 +18,17 @@ $nowp = date('r',strtotime("today")); // for RSS2 pubDate
 /*
  *
  * ATOM FEED
+ *
+ * NOTE: In $...meta don't use $website_url in the url to be created, otherwise fopen won't work for some reason
  */
-	$atommeta = fopen($website_url."users/".$username."-atom.xml", "w") or die("Unable to open or create ".$website_url."users/".$username."-atom.xml file");
+	$atommeta = fopen("users/".$username."-atom.xml", "w") or die("Unable to open or create users/".$username."-atom.xml file");
 
 	$atomstart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<feed xmlns=\"http://www.w3.org/2005/Atom\">\n\n\t<title>".$website_name." : ".$username."</title>\n\t<link rel=\"self\" href=\"".$website_url."users/".$username."/atom.xml\"/>\n\t<updated>".$nowa."</updated>\n\t<id>".$website_url."users/".$username."</id>\n\n";
 	fwrite($atommeta,$atomstart);
 
 	// if we have posts from this user, show the most recent 25.
 	if (post_quantity($posts) > 0) {
-		$pst_q = "SELECT * FROM posts WHERE post_status=\"PUBLIC\" AND post_type=\"POST\" AND user_id=\"".$userid."\" ORDER BY post_date DESC LIMIT 25";
+		$pst_q = "SELECT * FROM ".TBLPREFIX."posts WHERE post_status=\"PUBLIC\" AND post_type=\"POST\" AND user_id=\"".$userid."\" ORDER BY post_date DESC LIMIT 25";
 		$pst_query = mysqli_query($dbconn,$pst_q);
 		while ($pst_opt = mysqli_fetch_assoc($pst_query)) {
 			$postid		= $pst_opt['post_id'];
@@ -41,7 +43,7 @@ $nowp = date('r',strtotime("today")); // for RSS2 pubDate
 			while($by_opt = mysqli_fetch_assoc($by_query)) {
 				$byname		= $by_opt['user_name'];
 			}
-			$atompost = "\n\t<entry>\n\t\t<title type=\"html\">".$posttitle."</title>\n\t\t<link href=\"".$website_url."posts/".$postslug."\" />\n\t\t<id>".$website_url."posts/".$postslug."</id>\n\t\t<updated>".$posttime."</updated>\n\t\t<author>\n\t\t\t<name>".$byname."</name>\n\t\t</author>\n\t\t<content type=\"html\">".$posttext."</content>\n\t</entry>";
+			$atompost = "\n\t<entry>\n\t\t<title type=\"html\">".$posttitle."</title>\n\t\t<link href=\"".$website_url."posts/".$postslug."\" />\n\t\t<id>".$website_url."posts/".$postslug."</id>\n\t\t<updated>".$posttime."</updated>\n\t\t<author>\n\t\t\t<name>".$username."</name>\n\t\t</author>\n\t\t<content type=\"html\">".$posttext."</content>\n\t</entry>";
 			fwrite($atommeta,$atompost);
 
 		} // end while $pst_opt...
@@ -56,15 +58,18 @@ $nowp = date('r',strtotime("today")); // for RSS2 pubDate
 /*
  *
  * RSS 2.0 FEED
+ *
+ * NOTE: In $...meta don't use $website_url in the url to be created, otherwise fopen won't work for some reason
  */
 
-	$rss2meta = fopen($website_url."users/".$username."-rss2.xml", "w") or die("Unable to open or create ".$website_url."users/".$username."-rss2.xml file");
-	$rss2start = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<rss version=\"2.0\">\n<channel>\n\t<title>".$website_name." : ".$username."</title>\n\t<description>"._('The RSS feed of')." @".$username."@".short_url($website_url)."</description>\n\t<link>".$website_url."users/".$username."/</link><pubDate>".$nowp."</pubDate>\n\t<lastBuildDate>".$nowb."</lastBuildDate>\n\n";
+	$rss2meta = fopen("users/".$username."-rss2.xml", "w") or die("Unable to open or create users/".$username."-rss2.xml file");
+
+	$rss2start = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<rss version=\"2.0\">\n<channel>\n\t<title>".$website_name." : ".$username."</title>\n\t<description>"._('The RSS feed of')." @".$username."@".$_SERVER['SERVER_NAME']."</description>\n\t<link>".$website_url."users/".$username."/</link>\n\t<pubDate>".$nowp."</pubDate>\n\t<lastBuildDate>".$nowb."</lastBuildDate>\n\n";
 	fwrite ($rss2meta,$rss2start);
 
 	// if we have posts, display the most recent ones in a div on the right side of the page
 	if (post_quantity($posts) > 0) {
-		$rsspst_q = "SELECT * FROM posts WHERE post_status=\"PUBLIC\" AND post_type=\"POST\" AND user_id=\"".$userid."\" ORDER BY post_date DESC LIMIT 25";
+		$rsspst_q = "SELECT * FROM ".TBLPREFIX."posts WHERE post_status=\"PUBLIC\" AND post_type=\"POST\" AND user_id=\"".$userid."\" ORDER BY post_date DESC LIMIT 25";
 		$rsspst_query = mysqli_query($dbconn,$rsspst_q);
 		while ($rsspst_opt = mysqli_fetch_assoc($rsspst_query)) {
 			$rsspostid		= $rsspst_opt['post_id'];
@@ -79,7 +84,7 @@ $nowp = date('r',strtotime("today")); // for RSS2 pubDate
 			while($rssby_opt = mysqli_fetch_assoc($rssby_query)) {
 				$rssbyname		= $rssby_opt['user_name'];
 			}
-			$rss2post = "\n\t<item>\n\t\t<title>".$rssposttitle."</title>\n\t\t<link>".$website_url."posts/".$rsspostslug."\"</link>\n\t\t<pubDate>".$rssposttime."</pubDate>\n\t\t<author>@".$rssbyname."@".short_url($website_url)."</author>\n\t\t<description>".$rssposttext."</description>\n\t</item>";
+			$rss2post = "\n\t<item>\n\t\t<title>".$rssposttitle."</title>\n\t\t<link>".$website_url."posts/".$rsspostslug."\"</link>\n\t\t<pubDate>".$rssposttime."</pubDate>\n\t\t<author>@".$username."@".$_SERVER['SERVER_NAME']."</author>\n\t\t<description>".$rssposttext."</description>\n\t</item>";
 			fwrite($rss2meta,$rss2post);
 
 		} // end while $rsspst_opt...
--- pub/the-user-webfinger.php
+++ pub/the-user-webfinger.php
@@ -13,8 +13,11 @@
 /*
  *
  * CREATE WEBFINGER DOCUMENT
+ *
+ * NOTE: In $...meta don't use $website_url in the url to be created, otherwise fopen won't work for some reason
  */
-	$fingermeta = fopen($website_url.".well-known/webfinger?resource=acct:".$username."@".short_url($website_url), "w") or die("Unable to open or create ".$website_url.".well-known/webfinger?resource=acct:".$username."@".short_url($website_url)." file");
-	$fingerdata = "{\n\t\"subject\": \"acct:\"".$username."@".short_url($website_url)."\",\n\n\t\"links\": [\n\t\t{\n\t\t\t\"rel\": \"self\",\n\t\t\t\"type\": \"application/activity+json\",\n\t\t\t\"href\": \"".$website_url."users/".$username.".json\"\n\t\t}\n\t]\n}";
+	$fingermeta = fopen(".well-known/webfinger?resource=acct:".$username."@".$_SERVER['SERVER_NAME'], "w") or die("Unable to open or create .well-known/webfinger?resource=acct:".$username."@".$_SERVER['SERVER_NAME']." file");
+
+	$fingerdata = "{\n\t\"subject\": \"acct:".$username."@".$_SERVER['SERVER_NAME']."\",\n\n\t\"links\": [\n\t\t{\n\t\t\t\"rel\": \"self\",\n\t\t\t\"type\": \"application/activity+json\",\n\t\t\t\"href\": \"".$website_url."users/".$username.".json\"\n\t\t}\n\t]\n}";
 	fwrite($fingermeta,$fingerdata);
 ?>
