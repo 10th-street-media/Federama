bc4b9ea0508f9dee1ad9a192be27b1c176c71b2e
10sm <10sm@10thstreet.media>
2019-08-09 20:58:07 -0400

    Getting Webfinger working. Fix Issue #7

--- pub/the-user-feeds.php
+++ pub/the-user-feeds.php
@@ -21,9 +21,9 @@ $nowp = date('r',strtotime("today")); // for RSS2 pubDate
  *
  * NOTE: In $...meta don't use $website_url in the url to be created, otherwise fopen won't work for some reason
  */
-	$atommeta = fopen("users/".$username."-atom.xml", "w") or die("Unable to open or create users/".$username."-atom.xml file");
+	$atommeta = fopen("data/".$username."-atom.xml", "w") or die("Unable to open or create data/".$username."-atom.xml file");
 
-	$atomstart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<feed xmlns=\"http://www.w3.org/2005/Atom\">\n\n\t<title>".$website_name." : ".$username."</title>\n\t<link rel=\"self\" href=\"".$website_url."users/".$username."/atom.xml\"/>\n\t<updated>".$nowa."</updated>\n\t<id>".$website_url."users/".$username."</id>\n\n";
+	$atomstart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<feed xmlns=\"http://www.w3.org/2005/Atom\">\n\n\t<title>".$website_name." : ".$username."</title>\n\t<link rel=\"self\" href=\"".$website_url."data/".$username."-atom.xml\"/>\n\t<updated>".$nowa."</updated>\n\t<id>".$website_url."users/".$username."</id>\n\n";
 	fwrite($atommeta,$atomstart);
 
 	// if we have posts from this user, show the most recent 25.
@@ -62,7 +62,7 @@ $nowp = date('r',strtotime("today")); // for RSS2 pubDate
  * NOTE: In $...meta don't use $website_url in the url to be created, otherwise fopen won't work for some reason
  */
 
-	$rss2meta = fopen("users/".$username."-rss2.xml", "w") or die("Unable to open or create users/".$username."-rss2.xml file");
+	$rss2meta = fopen("data/".$username."-rss2.xml", "w") or die("Unable to open or create data/".$username."-rss2.xml file");
 
 	$rss2start = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<rss version=\"2.0\">\n<channel>\n\t<title>".$website_name." : ".$username."</title>\n\t<description>"._('The RSS feed of')." @".$username."@".$_SERVER['SERVER_NAME']."</description>\n\t<link>".$website_url."users/".$username."/</link>\n\t<pubDate>".$nowp."</pubDate>\n\t<lastBuildDate>".$nowb."</lastBuildDate>\n\n";
 	fwrite ($rss2meta,$rss2start);
