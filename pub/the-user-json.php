077dc95681cffc1156aedc9321d8634859a152ec
10sm <10sm@10thstreet.media>
2019-08-12 08:07:15 -0400

    Can display PGP key without giving an error

--- pub/the-user-json.php
+++ pub/the-user-json.php
@@ -18,6 +18,6 @@
  */
 	$jsonmeta = fopen("data/".$username.".json", "w") or die("Unable to open or create data/".$username.".json file");
 
-	$jsondata = "{\n\t\"@context\": [\n\t\t\"https://www.w3.org/ns/activitystreams\",\n\t\t\"https://w31d.org/security/v1\"\n\t],\n\n\t\"id\": \"".$website_url."data/".$username.".json\",\n\t\"type\": \"Person\",\n\t\"preferrefUsername\": \"".$username."\",\n\t\"inbox\": \"".$website_url."users/".$username."/inbox\",\n\n\t\"publicKey\": {\n\t\t\"id\": \"".$website_url."users/".$username."#public-key\",\n\t\t\"owner\": \"".$website_url."users/".$username."\",\n\t\t\"publicKeyPem\": \"".$userpubkey."\"\n\t}\n}";
+	$jsondata = "{\n\t\"@context\": [\n\t\t\"https://www.w3.org/ns/activitystreams\",\n\t\t\"https://w31d.org/security/v1\"\n\t],\n\n\t\"id\": \"".$website_url."data/".$username.".json\",\n\t\"type\": \"Person\",\n\t\"preferrefUsername\": \"".$username."\",\n\t\"inbox\": \"".$website_url."users/".$username."/inbox\",\n\n\t\"publicKey\": {\n\t\t\"id\": \"".$website_url."users/".$username."#public-key\",\n\t\t\"owner\": \"".$website_url."users/".$username."\",\n\t\t\"publicKeyPem\": \"".str_replace(array("\n", "\r"), '', $userpubkey)."\"\n\t}\n}";
 	fwrite($jsonmeta,$jsondata);
 ?>
