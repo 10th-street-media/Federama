d3ddec55d5fa02c40d4600c4a0051474de3b028e
10th Street Media <10sm@10thstreet.media>
2019-09-25 10:28:47 -0400

    Change order in which certain CSS files are called

--- pub/dash/admin/header.php
+++ pub/dash/admin/header.php
@@ -32,8 +32,8 @@
 	<link rel="shortcut icon" href="../images/favicon.ico">
 	<title><?php echo $pagetitle; ?></title>
 	<meta name="description" content="<?php echo $website_description; ?>">
-	<link href="../style/dash-style-default.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link  rel="stylesheet" type="text/css" href="../style/dash-style-default.css">
 </head>
 <body class="w3-theme-l5">
 	<div class="w3-top">
