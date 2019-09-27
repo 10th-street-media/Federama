5ebea9279a22aa0667f42d8cb5d3ee64106b390b
10th Street Media <10sm@10thstreet.media>
2019-09-27 10:39:39 -0400

    tabs to spaces

--- pub/nodeinfo.php
+++ pub/nodeinfo.php
@@ -14,21 +14,20 @@
  * NODEINFO CREATION
  */
 
-	$nodeinfometa = fopen(".well-known/nodeinfo", "w") or die("Unable to open or create nodeinfo file");
+    $nodeinfometa = fopen(".well-known/nodeinfo", "w") or die("Unable to open or create nodeinfo file");
+    $json0 = "{\"links\":[{\"rel\":\"http://nodeinfo.diaspora.software/ns/schema/2.0\",\"href\":\"".$website_url."nodeinfo/2.0\"}]}";
 
-	$json0 = "{\"links\":[{\"rel\":\"http://nodeinfo.diaspora.software/ns/schema/2.0\",\"href\":\"".$website_url."nodeinfo/2.0\"}]}";
-
-	// let's try to write to it.
-	fwrite($nodeinfometa,$json0);
-	fclose($nodeinfometa);
+    // let's try to write to it.
+    fwrite($nodeinfometa,$json0);
+    fclose($nodeinfometa);
 
 
 // create or update nodeinfo/2.0 file
-	$nodeinfo2 = fopen("nodeinfo/2.0", "w") or die("Unable to open or create nodeinfo 2.0 file");
+    $nodeinfo2 = fopen("nodeinfo/2.0", "w") or die("Unable to open or create nodeinfo 2.0 file");
 
-	$json2 = "{\"version\":\"2.0\",\"software\":{\"name\":\"federama\",\"version\":\"v0.2\"},\"protocols\":{\"inbound\":[],\"outbound\":[]},\"services\":{\"inbound\":[],\"outbound\":[atom1.0,rss2.0]},\"openRegistrations\":".$open.",\"usage\":{\"users\":{\"total\":".user_quantity($users).",\"activeHalfyear\":".users_half_year($sometimes_users).",\"activeMonth\":".users_past_month($active_users)."},\"localPosts\":".post_quantity($posts).",\"localComments\":},\"metadata\":{\"nodeName\":\"".$website_name."\"}}";
+    $json2 = "{\"version\":\"2.0\",\"software\":{\"name\":\"federama\",\"version\":\"v0.2\"},\"protocols\":{\"inbound\":[],\"outbound\":[]},\"services\":{\"inbound\":[],\"outbound\":[atom1.0,rss2.0]},\"openRegistrations\":".$open.",\"usage\":{\"users\":{\"total\":".user_quantity($users).",\"activeHalfyear\":".users_half_year($sometimes_users).",\"activeMonth\":".users_past_month($active_users)."},\"localPosts\":".post_quantity($posts).",\"localComments\":},\"metadata\":{\"nodeName\":\"".$website_name."\"}}";
 
-	fwrite($nodeinfo2,$json2);
-	fclose($nodeinfo2);
+    fwrite($nodeinfo2,$json2);
+    fclose($nodeinfo2);
 
 ?>
