2ae711d6adf125e504c2e60459162bfedbf1f1b0
10th Street Media <10sm@10thstreet.media>
2019-10-04 13:36:56 -0400

    Display categories and tags for posts

--- pub/the-post.php
+++ pub/the-post.php
@@ -33,10 +33,12 @@ if ($get_id != '') {
 		$postid		= $pst_opt['post_id'];
 		$posttitle	= $pst_opt['post_title'];
 		$postslug	= $pst_opt['post_slug'];
-		$postby		= $pst_opt['post_by'];
+		$postby		= $pst_opt['user_id'];
 		$posttime	= $pst_opt['post_date'];
 		$posttext	= htmlspecialchars_decode($pst_opt['post_text']);
-		$postpriv	= $pst_opt['post_privacy_level'];
+		$postpriv	= $pst_opt['post_status'];
+        $posttags   = $pst_opt['post_tags'];
+        $postcats   = $pst_opt['post_categories'];
 	}
 
 		$by_q = "SELECT * FROM ".TBLPREFIX."users WHERE user_id=\"".$postby."\"";
@@ -54,7 +56,9 @@ if ($get_id != '') {
 		$postby		= $pst_opt['user_id'];
 		$posttime	= $pst_opt['post_date'];
 		$posttext	= htmlspecialchars_decode($pst_opt['post_text']);
-		$postpriv	= $pst_opt['post_privacy_level'];
+		$postpriv	= $pst_opt['post_status'];
+        $posttags   = $pst_opt['post_tags'];
+        $postcats   = $pst_opt['post_categories'];
 	}
 
 		$by_q = "SELECT * FROM ".TBLPREFIX."users WHERE user_id=\"".$postby."\"";
@@ -82,7 +86,38 @@ include_once "includes/fed-header.php";
 		echo "\t\t\t\t<div class=\"w3-container w3-block\">\n";
 		echo htmlspecialchars_decode($posttext);
 		echo "\t\t\t\t</div>\n";
-		echo "\t\t\t</article>\n";
+if ($postcats !== NULL || $postcats !== '') {
+
+    echo "\t\t\t\t<div class=\"w3-container w3-block\">\n";
+    echo "\t\t\t\t\t<span>"._('Categories: ');
+    /**
+     * $postcats is a comma separated list in the form of a string
+     * We need to separate those and turn each category into a link
+     */
+    $cats = explode(',',$postcats);
+    foreach ($cats as $cat) {
+        echo "<a href=\"".$website_url."the-category.php?cat=".urlencode($cat)."\">".$cat."</a>&nbsp;&nbsp;";
+    }
+    echo "</span>\n";
+    echo "\t\t\t\t</div>\n";
+}
+
+if ($posttags !== NULL || $posttags !== '') {
+
+    echo "\t\t\t\t<div class=\"w3-container w3-block\">\n";
+    echo "\t\t\t\t\t<span>"._('Tags: ');
+    /**
+     * $posttags is a comma separated list in the form of a string
+     * We need to separate those and turn each tag into a link
+     */
+    $tags = explode(',',$posttags);
+    foreach ($tags as $tag) {
+        echo "<a href=\"".$website_url."the-tag.php?tag=".urlencode($tag)."\">".$tag."</a>&nbsp;&nbsp;";
+    }
+    echo "</span>\n";
+    echo "\t\t\t\t</div>\n";
+}
+            echo "\t\t\t</article>\n";
 ?>
 			<div class="w3-col w3-cell m3">&nbsp;</div>
 		</div> <!-- end THE GRID -->
