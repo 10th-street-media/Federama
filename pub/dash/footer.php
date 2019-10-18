ff73958427d870e4a6ea90b23e2d4cd794bdb76f
10th Street Media <10sm@10thstreet.media>
2019-10-18 09:33:31 -0400

    Moving to new site

--- pub/dash/footer.php
+++ pub/dash/footer.php
@@ -12,7 +12,7 @@
         </div> <!-- end The Grid -->
     </main> <!-- end The Container -->
     <footer class="w3-container w3-content w3-padding">
-        <span class="w3-padding"><?php echo _("Powered by "); ?><a href="https://github.com/10th-street-media/Federama">Federama</a></span>
+        <span class="w3-padding"><?php echo _("Powered by "); ?><a href="https://codeberg.org/10th-Street-Media/Federama">Federama</a></span>
     </footer>
 </body>
 </html>
